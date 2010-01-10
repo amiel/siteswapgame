5f164bac7387f50aaee23589485d44029cde15e6
Amiel Martin <amiel.martin@gmail.com>
2010-01-10 04:56:03 -0800

    Add files from template lib

---
+++ public/javascripts/lib/base.js
@@ -0,0 +1,71 @@
+/*
+ * base.js
+ * Amiel Martin
+ * 2009-10-14
+ */
+
+// base.js provides some useful functions for global use under the namespace Base,
+// as well as a namespace for variables to be registered from the server
+
+
+var Base = {};
+
+// register a variable for use within the Base namespace
+// there is an optional scope
+// You may want to use the +js_var+ helper found in javascript_helper.rb
+Base.register_variable = function(key, value, scope) {
+    if (scope) {
+        if (typeof Base[scope] === "undefined") Base[scope] = {};
+        Base[scope][key] = value;
+    } else {
+        Base[key] = value;
+    }
+};
+Base.reg = Base.register_variable;
+
+
+// the following section provides an abstraction to the Firebug console
+//
+// you may want to do something like this: <% js_var :DEBUG, !Rails.env.production? %>
+
+
+// apply is used way to much here //
+
+// log to the firebug console if window.console is available
+Base.console = function(method) {
+	if (Base.DEBUG && window.console && window.console[method])
+		window.console[method].apply(null, Array.prototype.slice.apply(arguments, [1]));
+};
+
+// firebugx
+// if (!("console" in window) || !("firebug" in console)) {
+//     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+//     "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+// 
+//     window.console = {};
+//     for (var i = 0; i < names.length; ++i)
+//         window.console[names[i]] = function() {}
+// }
+
+// methods for logging
+// this gives us Base.log, Base.console.log, Base.debug, etc
+(function(methods){
+	for (i in methods)
+		(function(method_name){
+			Base[method_name] = function() {
+				if (Base.DEBUG) Base.console.apply(null, [ method_name, 'Base', "[" + (new Date).toLocaleTimeString() + "]" ].concat(Array.prototype.slice.apply(arguments)));
+			};
+		})(methods[i]);
+})(['log', 'debug', 'info', 'warn', 'error']);
+
+// all other firebug helpful methods
+// this gives us Base.console.assert, Base.console.dir, etc
+(function(methods){
+	for (i in methods)
+		(function(method_name){
+			Base.console[method_name] = function() {
+				if (Base.DEBUG) Base.console.apply(null, [ method_name ].concat(Array.prototype.slice.apply(arguments)));
+			};
+		})(methods[i]);
+})(['log', 'debug', 'info', 'warn', 'error', 'assert', 'dir', 'dirxml', 'trace', 'group', 'groupEnd', 'time', 'timeEnd', 'profile', 'profileEnd', 'count']);
+
