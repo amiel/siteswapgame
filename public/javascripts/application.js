be7a596e03e642f051d601729f21a2caa0bcf0b4
Amiel Martin <amiel.martin@gmail.com>
2010-01-10 19:19:34 -0800

    WE HAVE A GAME

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,15 @@
 
+// not used anymore
 function is_valid(values) {
-	var to_check,
-		places = [],
+	var catch_position,
+		catches = [],
 		period = values.length;
 		
 	for (var i = 0; i < period; ++i) {
-		to_check = (i + values[i]) % period;
-		if (places[to_check])
+		catch_position = (i + values[i]) % period;
+		if (catches[catch_position])
 			return false;
-		places[to_check] = true;
+		catches[catch_position] = true;
 	}
 	
 	return true;
@@ -20,46 +21,85 @@ function numbers_from_string(s) {
 	return $.map(s.split(''), function(i){ return parseFloat(i, 10); });
 }
 
+
 function new_game() {
-	var numbers = [], sorted_anagram;
+	var sorted_anagram, current_possible_answers;
+
+
+	function correct_answer(attempt)  {
+		for (var a in current_possible_answers) {
+			if (current_possible_answers[a] == attempt) return true;
+		}
+		return false;
+	}
 	
-	for (var i=0; i < 6; ++i) numbers.push(Math.round(Math.random() * 9));
+	function output_result(to, value) {
+		return $(to).parent().find('.result').html(value);
+	}
+	
+	function have_duplicate_answer(input, attempt) {
+		var found_duplicate_answer = false;
+		input.parent().parent().find('input').not(input).each(function(index) {
+			if ($(this).val() == attempt) found_duplicate_answer = true;
+		});
+		return found_duplicate_answer;
+	}
 
-	var sum = 0;
-	$.each(numbers, function(i, e) { sum += e; });
-	numbers.push(7 - (sum % 7));
+	function setup_handlers() {
+		$('#attempts input').keyup(function() {
+			var input = $(this),
+				attempt = $(this).val();
+			
+			input.parent().find('.result').empty();
+			if (attempt.length != 7) return;
 
-	$('#original').html(numbers.join(''));
-	sorted_anagram = numbers.sort().join('');
+			if (correct_answer(attempt)) {
+				if (have_duplicate_answer(input, attempt)) {
+					output_result(input, 'duplicate answer');
+				} else {
+					output_result(input, 'ok');
+					input.attr('readonly', 'readonly');
+					input.parent().next().find('input').focus();
+				}
+			} else {
+				output_result(input, 'incorrect');
+			}
+		});
+	}
 
-	Base.log(siteswap_anagrams[sorted_anagram]);
-	// for(var answer in siteswap_anagrams[sorted_anagram]) {
-	// 	$('#answers').append('<li>' + siteswap_anagrams[sorted_anagram][answer] + '</li>');
-	// }
+	function create_siteswap() {
+		var period = 7, numbers = [];
+		// start with random numbers
+		for (var i=0; i < period-1; ++i) numbers.push(Math.round(Math.random() * 9));
+
+		// determine the last number to make the sum divisible by the period
+		var sum = 0;
+		$.each(numbers, function(i, e) { sum += e; });
+		numbers.push(period - (sum % period));
+		return numbers;
+	}
+
+	function setup_game() {
+		var numbers = create_siteswap();
+		$('#problem').html(numbers.join(''));
+		$('#attempts').empty();
+		sorted_anagram = numbers.sort().join('');
+		current_possible_answers = siteswap_anagrams[sorted_anagram];
+	
+		Base.log(current_possible_answers);
+		$.each(current_possible_answers, function() {
+			$('#attempts').append('<li><input type="text" /><span class="result"></span></li>');
+		});
+		
+		setup_handlers();
+	}
+
+	setup_game();
+	
+	return false;
 }
 
 $(document).ready(function() {
-
-	$('button#give_anagrams').click(function() {
-		var value = $('input').val(),
-			numbers = numbers_from_string(value),
-			sorted_anagram = numbers.sort().join('');
-			
-		window.console.log('give anagrams', numbers);
-		for(var answer in siteswap_anagrams[sorted_anagram]) {
-			window.console.log(sorted_anagram, siteswap_anagrams[sorted_anagram], siteswap_anagrams[sorted_anagram][answer]);
-			$('#results').prepend('<li>' + siteswap_anagrams[sorted_anagram][answer] + '</li>');
-		}
-	});
-	
-	$('button#try').click(function() {
-		var value = $('input').val(),
-			values = numbers_from_string(value);
-		
-		if (is_valid(values))
-			$('#results').prepend('<li>' + value + ': valid</li>');
-		else
-			$('#results').prepend('<li>' + value + ': invalid</li>');
-		
-	});
+	$('#new_game').click(new_game);
+	$('#container').submit(function() { return false; });
 });
--- public/javascripts/lib/base.js
+++ public/javascripts/lib/base.js
@@ -23,6 +23,27 @@ Base.register_variable = function(key, value, scope) {
 };
 Base.reg = Base.register_variable;
 
+Base.initializers = {};
+
+Base.init = function() {
+	if (arguments.length == 1) {
+		var to_init = arguments[0],
+			scope = $(to_init).not('.initialized').addClass('initialized');
+		$.each(Base.initializers[to_init], function(i, f) { f.apply(scope); });
+	} else {
+		var collection = (arguments.length == 0) ? Base.initializers : arguments;
+		$.each(collection, function(i, e) { if (typeof i === 'string') Base.init(i); else Base.init(e); });
+	}
+};
+
+Base.register_initializer = function(scope, lambda) {
+	if (!Base.initializers[scope]) Base.initializers[scope] = [];
+	Base.initializers[scope].push(lambda);
+};
+
+$(function(){ Base.init(); });
+
+
 
 // the following section provides an abstraction to the Firebug console
 //
@@ -34,18 +55,8 @@ Base.reg = Base.register_variable;
 // log to the firebug console if window.console is available
 Base.console = function(method) {
 	if (Base.DEBUG && window.console && window.console[method])
-		window.console[method].apply(null, Array.prototype.slice.apply(arguments, [1]));
+		$.browser.mozilla ? window.console[method].apply(null, Array.prototype.slice.apply(arguments, [1])) : window.console[method](arguments);
 };
-
-// firebugx
-// if (!("console" in window) || !("firebug" in console)) {
-//     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
-//     "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
-// 
-//     window.console = {};
-//     for (var i = 0; i < names.length; ++i)
-//         window.console[names[i]] = function() {}
-// }
 
 // methods for logging
 // this gives us Base.log, Base.console.log, Base.debug, etc
@@ -69,3 +80,6 @@ Base.console = function(method) {
 		})(methods[i]);
 })(['log', 'debug', 'info', 'warn', 'error', 'assert', 'dir', 'dirxml', 'trace', 'group', 'groupEnd', 'time', 'timeEnd', 'profile', 'profileEnd', 'count']);
 
+
+// trace ajax operations when "App.DEBUG == true"
+if (Base.DEBUG) $(window).bind("ajaxStart ajaxSend ajaxComplete ajaxSuccess", Base.debug);
