f8daf17a5af7b3362dc903c925fa46a94eed6a71
Amiel Martin <amiel@carnesmedia.com>
2012-10-19 11:16:01 -0700

    Generate rails 3.2, setup Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siteswapgame::Application.config.secret_token = 'b284334283ae23643c7d90e101abab5efe77bc487cc93393c4556423456c546884dbc29de07ac4b33c3a1d97872fd1832f99d292c191e0d5ff4c211f52ea7df0'
